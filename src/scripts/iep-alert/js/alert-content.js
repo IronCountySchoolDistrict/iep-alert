937c4e096013693995e61533df01fad31a3d14a7
Nathan Jones <Purkinje90@gmail.com>
2015-10-06 09:08:38 -0600

    Use es6 module style imports. Changed js imports from html to use require() calls

--- src/scripts/iep-alert/js/alert-content.js
+++ src/scripts/iep-alert/js/alert-content.js
@@ -1,19 +1,32 @@
 /* global psData, require */
-require(["underscore", "jquery"], function(_, $) {
+import $ from 'jquery';
+import _ from 'underscore';
 
-    $.getJSON('/ws/schema/table/U_SPED_STUDENTS2/' + psData.studentdcid + '?projection=*', function (alertResp) {
-        var alert = alertResp.tables.u_sped_students2;
-        var template = $('#alert-content-template').html();
-        var select = $('#psDialog');
-        if (alert.additional_adaptations) {
-          alert.additional_adaptations = alert.additional_adaptations.replace(/\n/g, "<br>");
-        }
-        var renderedTemplate = _.template(template, {alert: alert});
-        select.html(renderedTemplate);
+export default function() {
+  if (getPortal() === 'admin') {
+    alertUrl = `/ws/schema/table/U_SPED_STUDENTS2/${psData.studentdcid}?projection=*`;
+  } else {
+    alertUrl = `/teachers/alerts/sped-alert.json.html?studentsdcid=${psData.studentdcid}`;
+  }
 
-        // When the dialog is first rendered, there isn't any content in it, so the centering is based on
-        // the empty dialog box. When content is put into it, it doesn't automatically recenter itself,
-        // so calling this function will recenter the dialog box.
-        $('#psDialog').dialog('option', 'position', 'center');
-    });
-});
+  $.getJSON(alertUrl, function (alertResp) {
+      var alert;
+      if (getPortal() === 'admin') {
+        alert = alertResp.tables.u_sped_students2;
+      } else {
+        alert = alertResp;
+      }
+      var template = $('#alert-content-template').html();
+      var select = $('#psDialog');
+      if (alert.additional_adaptations) {
+        alert.additional_adaptations = alert.additional_adaptations.replace(/\n/g, "<br>");
+      }
+      var renderedTemplate = _.template(template, {alert: alert});
+      select.html(renderedTemplate);
+
+      // When the dialog is first rendered, there isn't any content in it, so the centering is based on
+      // the empty dialog box. When content is put into it, it doesn't automatically recenter itself,
+      // so calling this function will recenter the dialog box.
+      $('#psDialog').dialog('option', 'position', 'center');
+  });
+}
--- src/scripts/iep-alert/js/alert.js
+++ src/scripts/iep-alert/js/alert.js
@@ -1,49 +1,76 @@
 /*global require, $, _, psData*/
+'use strict';
+import $ from 'jquery';
+import _ from 'underscore';
 
-require(['underscore', 'jquery'], function (_, $) {
-        'use strict';
-        $.getJSON('/ws/schema/table/U_SPED_STUDENTS2/' + psData.studentdcid + '?projection=*', function (alertResp) {
-            var alert = alertResp.tables['u_sped_students2'];
-            var alertTemplate = $('#alert-template').html();
-            var renderedTemplate = _.template(alertTemplate, {});
-            var pageHeader = $('h1');
-            var alerts = pageHeader.find('.dialogM');
-            // If there are alerts already displayed
-            var select;
-            if ( !($.isEmptyObject(alerts[0])) ) {
-                select = alerts.last();
-                $(renderedTemplate).insertAfter(select);
-            } else {
-                select = pageHeader.contents().eq(0);
-                $(renderedTemplate).insertAfter(select);
-            }
+export var fetchAlert = function() {
+  var alertUrl;
+  if (getPortal() === 'admin') {
+    alertUrl = `/ws/schema/table/U_SPED_STUDENTS2/${psData.studentdcid}?projection=*`;
+  } else {
+    alertUrl = `/teachers/alerts/sped-alert.json.html?studentsdcid=${psData.studentdcid}`;
+  }
+  fetch(alertUrl, {
+      credentials: 'include'
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(alertResp) {
+      var alert;
+      if (getPortal() === 'admin') {
+        alert = alertResp.tables['u_sped_students2'];
+      } else {
+        alert = alertResp;
+      }
+      var alertTemplate = $('#alert-template').html();
+      var renderedTemplate = _.template(alertTemplate, {});
+      var pageHeader = $('h1');
+      var alerts = pageHeader.find('.dialogM');
+      // If there are alerts already displayed
+      var select;
+      if (!$.isEmptyObject(alerts[0])) {
+        select = alerts.last();
+        $(renderedTemplate).insertAfter(select);
+      } else {
+        select = pageHeader.contents().eq(0);
+        $(renderedTemplate).insertAfter(select);
+      }
 
-            if (alert.caseload) {
-                $('#alert-img').css({'display': 'inline'});
-            }
-
-            // TODO: Refactor this so this code doesn't have to run again after the alert is added.
-            alerts = pageHeader.find('.dialogM');
-
-            // Find the span element that contains elements with class .dialogM (alerts),
-            // and remove "float: right" css rule from that span.
-            // Note that not all pages with alerts place alerts in a span tag.
-            var spanAlerts = pageHeader.find('span .dialogM');
-            if ( !($.isEmptyObject(spanAlerts)[0]) ) {
-                var alertSpan = spanAlerts.eq(0).parent();
-                if (alertSpan.css('float') == 'right') {
-                    alertSpan.css('float', 'none');
-                }
-            }
-
-            $.each(alerts, function(index, elem) {
-                $(elem).css({'visibility': 'visible'});
-            });
-
-            $('#student-service-alert').on('click', function(event) {
-                var template = $('#alert-balance~(studentfrn)').html();
-                var renderedTemplate = _.template(template, {});
-                $('#psDialog').html(renderedTemplate);
-            });
+      if (alert.caseload) {
+        $('#alert-img').css({
+          'display': 'inline'
         });
+      }
+
+      // TODO: Refactor this so this code doesn't have to run again after the alert is added.
+      alerts = pageHeader.find('.dialogM');
+
+      // Find the span element that contains elements with class .dialogM (alerts),
+      // and remove "float: right" css rule from that span.
+      // Note that not all pages with alerts place alerts in a span tag.
+      var spanAlerts = pageHeader.find('span .dialogM');
+      if (!$.isEmptyObject(spanAlerts)[0]) {
+        var alertSpan = spanAlerts.eq(0).parent();
+        if (alertSpan.css('float') == 'right') {
+          alertSpan.css('float', 'none');
+        }
+      }
+
+      $.each(alerts, function(index, elem) {
+        $(elem).css({
+          'visibility': 'visible'
+        });
+      });
+
+      $('#student-service-alert').on('click', function(event) {
+        var template = $('#alert-balance~(studentfrn)').html();
+        var renderedTemplate = _.template(template, {});
+        $('#psDialog').html(renderedTemplate);
+      });
+    })
+    .catch(function(ex) {
+      console.dir(ex);
+      console.log('student alert not found');
     });
+};
